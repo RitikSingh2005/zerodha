d0e6dcb66b015e7d022ae9abadcc6645ba22dca2
Ritik Singh <hritiksingh5002@gmail.com>
2026-02-13 15:06:28 +0530

    in

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,15 +3,15 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 
-import HomePage from "./landing_page/home/HomePage";
-import AboutPage from "./landing_page/about/AboutPage";
-import ProductPage from "./landing_page/products/ProductsPage";
-import PricingPage from "./landing_page/pricing/PricingPage";
-import SupportPage from "./landing_page/support/SupportPage";
+import HomePage from "./landing_page/home/HomePage.jsx";
+import AboutPage from "./landing_page/about/AboutPage.jsx";
+import ProductPage from "./landing_page/products/ProductsPage.jsx";
+import PricingPage from "./landing_page/pricing/PricingPage.jsx";
+import SupportPage from "./landing_page/support/SupportPage.jsx";
 
-import Navbar from "./landing_page/Navbar";
-import Footer from "./landing_page/Footer";
-import NotFound from "./landing_page/NotFound";
+import Navbar from "./landing_page/Navbar.jsx";
+import Footer from "./landing_page/Footer.jsx";
+import NotFound from "./landing_page/NotFound.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
