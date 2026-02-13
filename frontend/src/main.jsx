ab6a7909d8708662a64d3e422e9ca45c3aa2ba09
Ritik Singh <hritiksingh5002@gmail.com>
2026-02-13 14:57:38 +0530

    initial commit
    Removed Signup route and fixed NotFound import.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 
 import HomePage from "./landing_page/home/HomePage";
-import Signup from "./landing_page/signup/Signup";
 import AboutPage from "./landing_page/about/AboutPage";
 import ProductPage from "./landing_page/products/ProductsPage";
 import PricingPage from "./landing_page/pricing/PricingPage";
@@ -12,14 +11,14 @@ import SupportPage from "./landing_page/support/SupportPage";
 
 import Navbar from "./landing_page/Navbar";
 import Footer from "./landing_page/Footer";
-import NotFound from "./landing_page/NotFoun";
+import NotFound from "./landing_page/NotFound";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Navbar/>
     <Routes>
       <Route path="/" element={<HomePage/>}/>
-      <Route path="/signup" element={<Signup />} />
+
       <Route path="/about" element={<AboutPage />} />
       <Route path="/product" element={<ProductPage />} />
       <Route path="/pricing" element={<PricingPage />} />
