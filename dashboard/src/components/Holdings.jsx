9d9b5d05f32ad06b52b375e16b4ae971fe97f13b
Ritik Singh <hritiksingh5002@gmail.com>
2026-02-13 14:23:23 +0530

    Update API endpoint for fetching all holdings

--- dashboard/src/components/Holdings.jsx
+++ dashboard/src/components/Holdings.jsx
@@ -11,7 +11,7 @@ const Holdings = () => {
   const[allHoldings,setAllHoldings]=useState([]);
 
   useEffect(()=>{ 
-     axios.get("http://localhost:3002/allHoldings").then((res)=>{
+     axios.get("https://zerodha-ufw8.onrender.com/allHoldings").then((res)=>{
       console.log(res.data);  
       setAllHoldings(res.data);
      })
